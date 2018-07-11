bd4dd0fe74f89eadca396714ffee7571c7fc82cd
Leonardo Jimenez <jimenezleonardop@gmail.com>
2018-07-11 19:24:02 -0400

    Commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { NgForm } from '../../../../node_modules/@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+
+  constructor(public postService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postService.addPost(form.value.title, form.value.content);
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   {title: 'First', content: 'Bla'},
+  //   {title: 'First2', content: 'Bla2'},
+  //   {title: 'First3', content: 'Bla3'},
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
